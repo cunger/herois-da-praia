40dc9a6422e1ce0b4cd7219e4f27641982306e2e
Christina Unger <christina@mercury.ai>
2018-08-31 10:48:36 +0200

    add: display error when creating scope without date

--- app/controllers/scopes_controller.rb
+++ app/controllers/scopes_controller.rb
@@ -1,15 +1,22 @@
 class ScopesController < ApplicationController
+  # Scopes are shared among beach clean and whale watching routes.
+  # The destination parameter ensures that redirecting from scope goes
+  # to the intended destination:
+  # /beachclean -> /scope/new?for=beachclean -> /scope/:id/items
+  # /whalewatch -> /scope/new?for-whalewatch -> /scope/:id/observations
+
   def new
     build_scope
-    @for = params[:for]
+    set_destination
   end
 
   def create
     build_scope
 
-    if @scope.save
+    if save_scope
       redirect_to_logging_page
     else
+      set_destination
       render new_scope_path
     end
   end
@@ -25,8 +32,21 @@ class ScopesController < ApplicationController
     scope_params ? scope_params.permit(:date, :place, :user_id) : {}
   end
 
+  def save_scope
+    flash.now[:date_missing] = true unless @scope[:date]
+    @scope.save
+  end
+
+  def set_destination
+    @for = params[:for] || destination
+  end
+
+  def destination
+    params[:scope][:for]
+  end
+
   def redirect_to_logging_page
-    redirect_to items_scope_path(@scope) if params[:scope][:for] == 'beachclean'
-    redirect_to scope_observations_path(@scope) if params[:scope][:for] == 'whalewatch'
+    redirect_to items_scope_path(@scope) if destination == 'beachclean'
+    redirect_to scope_observations_path(@scope) if destination == 'whalewatch'
   end
 end
--- spec/features/start_logging_spec.rb
+++ spec/features/start_logging_spec.rb
@@ -4,7 +4,7 @@ require_relative '../support/new_scope_form'
 feature 'Start logging' do
   let(:new_scope_form) { NewScopeForm.new }
 
-  scenario 'start a new beach clean' do
+  scenario 'a new beach clean' do
     new_scope_form.visit_from_beachclean.fill_in_with(
       date: '2018-12-31',
       place: 'Paindane'
@@ -16,7 +16,7 @@ feature 'Start logging' do
     expect(page).to have_current_path(items_scope_path(Scope.last.id))
   end
 
-  scenario 'start a new whale watch' do
+  scenario 'a new whale watch' do
     new_scope_form.visit_from_whalewatch.fill_in_with(
       date: '2019-01-01',
       place: 'Other'
@@ -28,13 +28,13 @@ feature 'Start logging' do
     expect(page).to have_current_path(scope_observations_path(Scope.last.id))
   end
 
-  scenario 'start a new scope without filling in the date field is invalid' do
+  scenario 'a new scope without filling in the date field is invalid' do
     new_scope_form.visit_from_beachclean.fill_in_with().submit
 
     expect(Scope.last).to be_nil
   end
 
-  scenario 'start a new scope without filling in the place field is fine' do
+  scenario 'a new scope without filling in the place field is fine' do
     new_scope_form.visit_from_beachclean.fill_in_with(
       date: '2018-01-01'
     ).submit
