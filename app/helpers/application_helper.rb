c623fa7bb0c3048f2a9a667aead126699b9c1c2a
Christina Unger <christina@mercury.ai>
2018-11-17 12:42:33 +0100

    fix: pluralization of plastic fragments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,13 @@ module ApplicationHelper
   def pretty_print_place(place)
     place.nil? ? 'unknown' : place.name
   end
+
+  def to_plural(amount, string)
+    # TODO This probably needs generalization at some point.
+    if amount > 1 && string.include?('fragment')
+      "#{amount} #{string.gsub('fragment', 'fragments')}"
+    else
+      pluralize(amount, string)
+    end
+  end
 end
